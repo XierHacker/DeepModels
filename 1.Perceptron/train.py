4dae78a2154f4fce8d305c54733ce762856076e8
XierHacker <xierhacker@163.com>
2018-05-05 16:03:44 +0800

    finish test.py and train.py

--- 1.Perceptron/train.py
+++ 1.Perceptron/train.py
@@ -10,11 +10,12 @@ from utility import preprocessing
 MAX_EPOCH=10
 BATCH_SIZE=20
 LEARNING_RATE=0.001
+MODEL_SAVING_PATH="./saved_models/model.ckpt"
 
+#load data
 X_train,y_train,X_valid,y_valid,X_test=preprocessing.load_mnist(path="../../data/mnist/")
 train_samples=X_train.shape[0]
 valid_samples=X_valid.shape[0]
-
 
 def train():
     #data placeholder
@@ -30,12 +31,16 @@ def train():
     logits=model.forward(X_p,regularizer)           #[batch_size,10]
     pred=tf.argmax(input=logits,axis=-1)            #[batch_size,]
 
-    #collect=tf.get_collection(key="regularized")
-    #print(collect)
-    loss=tf.losses.softmax_cross_entropy(onehot_labels=y_hot_p,logits=logits)
+    #collect_list=tf.get_collection(key="regularized")
+    #print("collect_list.shape",collect_list)
+    loss=tf.losses.softmax_cross_entropy(onehot_labels=y_hot_p,logits=logits)+\
+                        tf.add_n(inputs=tf.get_collection(key="regularized"))
     optimizer=tf.train.AdamOptimizer(learning_rate=LEARNING_RATE).minimize(loss=loss)
 
     init=tf.global_variables_initializer()
+
+    #Saver class
+    saver=tf.train.Saver()
 
     with tf.Session() as sess:
         sess.run(init)
@@ -52,11 +57,12 @@ def train():
                 )
                 accu=accuracy_score(y_true=y_train[j * BATCH_SIZE:(j + 1) * BATCH_SIZE],y_pred=prediction)
                 accus.append(accu)
-                #print(y_train[j * BATCH_SIZE:(j + 1) * BATCH_SIZE])
-                #print(prediction)
                 ls.append(l)
 
-            print(i, ":", "loss:",sum(ls) / len(ls),"accuracy:",sum(accus)/len(accus))
+            print("Epoch:", i, "-loss:",sum(ls) / len(ls),"-accuracy:",sum(accus)/len(accus))
+
+            #sava models
+            saver.save(sess=sess,save_path=MODEL_SAVING_PATH,global_step=i)
 
 
 if __name__=="__main__":
